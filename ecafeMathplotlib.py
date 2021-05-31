d452def567f8628048a6d2b2704b2e243619b928
Justine U <ujusine@yahoo.com>
2021-06-01 01:46:05 +0800

    Updated added visualization to Active orders

--- ecafeMathplotlib.py
+++ ecafeMathplotlib.py
@@ -19,63 +19,75 @@ itemName = pd.read_sql_table("ordered_items", engine, columns=[
                          'item'])
 date = pd.read_sql_table("ordered_items", engine, columns=['date_current',
                          'hourly_sales', 'quantity_sold'])
+day = pd.read_sql_table("ordered_items", engine, columns=[
+                         'date_current'])
+hourly = pd.read_sql_table("ordered_items", engine, columns=[
+                         'hourly_sales'])
+# hourlySold = pd.read_sql_table("ordered_items", engine, columns=['quantity_sold'])
+
+dateGroup = date.groupby(['date_current','hourly_sales'])['quantity_sold'].sum()
 
 # Group data under 'item' column
 # Sum data in 'quantity_sold' column
 mergedItem = data.groupby('item')['quantity_sold'].sum()
 
-arrItemName = np.array(itemName)
+# BAR GRAPH ACTIVE HOURS
+# df = pd.DataFrame(data[['date_current', 'hourly_sales', 'quantity_sold']])
+# df_pivot = pd.pivot_table(
+#     df, 
+#     values="quantity_sold",
+#     index="date_current",
+#     columns="hourly_sales", 
+#     aggfunc=np.sum,
+#     fill_value=0,
+# )
 
-itemLabel = np.concatenate(arrItemName)
-# print(mergedItem)
-# print(np.unique(itemLabel))
-# print(mergedItem.to_numpy())
+# print(dateGroup)
+# ax = df_pivot.plot(kind="bar")
+# fig = ax.get_figure()
+# fig.set_size_inches(10, 5)
+# ax.set_title('Ordered items', fontsize='18', fontweight='bold', color ='grey')
+# ax.set_xlabel("Date", fontsize='14', fontweight='bold', color ='grey')
+# ax.set_ylabel("Total item sold", fontsize='14', fontweight='bold', color ='grey')
+# plt.xticks(rotation=0)
+# plt.tight_layout()
+# plt.show()
 
-newData = {
-        'itemName': np.unique(itemLabel),
-        'q_value' : mergedItem.to_numpy()
-}
+#BAR GRAPH PER ITEM SOLD
+# itemSold = {
+#         'item': np.unique(np.concatenate(np.array(itemName))),
+#         'quantity' : mergedItem.to_numpy()
+# }
 
-df = pd.DataFrame.from_dict(newData)
-ascending = df.sort_values(by=['q_value'],ascending=False)
+# df3 = pd.DataFrame.from_dict(itemSold)
+# ascending = df3.sort_values(by=['quantity'],ascending=False)
 
-print(df)
+# print(df3)
 
-#BAR GRAPH
-fig1, ax = plt.subplots(figsize =(16, 9))
+# fig1, ax = plt.subplots(figsize =(10, 6))
 
-ax.set_title('Ordered items', fontsize='28', fontweight='bold', color ='grey', pad=20)
-ax.set_ylabel('Items', fontsize='14', fontweight='bold', color ='grey', labelpad=10)
-ax.set_xlabel('Quantity sold', fontsize='14', fontweight='bold', color ='grey', labelpad=10)
+# ax.set_title('Ordered items', fontsize='18', fontweight='bold', color ='grey')
+# ax.set_ylabel('Items', fontsize='14', fontweight='bold', color ='grey')
+# ax.set_xlabel('Quantity sold', fontsize='14', fontweight='bold', color ='grey')
 
-ax.grid(b = True, color ='grey',
-        linestyle ='-.', linewidth = 0.5,
-        alpha = 0.2)
+# ax.grid(b = True, color ='grey',
+#         linestyle ='-.', linewidth = 0.5,
+#         alpha = 0.2)
 
-ax.invert_yaxis()
+# ax.invert_yaxis()
 
-ax.xaxis.set_ticks_position('none')
-ax.yaxis.set_ticks_position('none')
+# ax.xaxis.set_ticks_position('none')
+# ax.yaxis.set_ticks_position('none')
 
-ax.xaxis.set_tick_params(pad = 5)
-ax.yaxis.set_tick_params(pad = 10)
+# ax.xaxis.set_tick_params(pad = 5)
+# ax.yaxis.set_tick_params(pad = 10)
 
-ax.set_xlim(0, df.q_value.max() * 1.25)
+# ax.set_xlim(0, df3.quantity.max() * 1.2)
 
-rects = ax.barh(ascending.itemName, ascending.q_value, color="coral")
+# rects = ax.barh(ascending.item, ascending.quantity, color="coral")
 
-for Y,X in enumerate(ascending.q_value):
-        ax.annotate("{:.1f}%".format(X * 100/np.sum(ascending.q_value)), xy=(X,Y), color='grey', fontsize='10', va='center')
+# for Y,X in enumerate(ascending.quantity):
+#         ax.annotate("{:.1f}%".format(X * 100/np.sum(ascending.quantity)), xy=(X,Y), color='grey', fontsize='10', va='center')
 
-plt.show()
-
-#PIE CHART
-# labels = np.unique(itemLabel)
-# sizes = mergedItem.to_numpy()
-
-# ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-#         startangle=90)
-# ax1.axis('equal')
-# ax1.legend()
-
+# plt.tight_layout()
 # plt.show()
